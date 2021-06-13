0a3b8269c7676e4bf7ca001dc716836dcdc9b9d8
vahid-dev <vahidgh.vgh@gmail.com>
2021-06-13 15:00:36 +0430

    Delete currentPointIndex

--- contracts/dbETHToken.sol
+++ contracts/dbETHToken.sol
@@ -9,11 +9,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract dbETHToken is ERC20, AccessControl{
 
-	uint256 public currentPointIndex = 0;
-
 	bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 	bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
-	bytes32 public constant CURRENT_POINT_INDEX_SETTER_ROLE = keccak256("CURRENT_POINT_INDEX_SETTER_ROLE");
 
 	constructor() ERC20("bonded ethereum", "dbETH") {
 		_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);	
@@ -29,11 +26,6 @@ contract dbETHToken is ERC20, AccessControl{
         require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
         _burn(from, amount);
     }
-
-	function setCurrentPointIndex(uint256 _currentPointIndex) public {
-		require(hasRole(CURRENT_POINT_INDEX_SETTER_ROLE, msg.sender), "Caller is not a currentPointIndex setter");
-		currentPointIndex = _currentPointIndex;
-	}
 
 }
 //Dar panah khoda
